546443c7d75ac38be4a5b2075730411db62a7dd0
Obed-Rosales <44353782+Obed-Rosales@users.noreply.github.com>
2019-01-24 12:07:57 -0600

    Version 1 de rikito

---
+++ administrator/logs/joomla_update.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2019-01-24 14:46:28 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2019-01-24T14:46:28+00:00	INFO ::1	update	COM_JOOMLAUPDATE_UPDATE_LOG_DELETE_FILES
